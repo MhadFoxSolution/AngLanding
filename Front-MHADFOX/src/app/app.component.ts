a0ada6ce92ad5607ed173f451dba042c184cb907
AldoGzlVictoria <aldo.vic1408@hotmail.com>
2025-02-03 17:26:29 -0600

    Se genero un encabezado para nuestra presentacion como empresa

--- Front-MHADFOX/src/app/app.component.ts
+++ Front-MHADFOX/src/app/app.component.ts
@@ -5,6 +5,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
@@ -15,7 +16,8 @@ import { FooterComponent } from './footer/footer.component';
     MatButtonModule,
     MatToolbarModule,
     NavbarComponent,
-    FooterComponent   
+    FooterComponent,
+    HeaderComponent   
   ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
