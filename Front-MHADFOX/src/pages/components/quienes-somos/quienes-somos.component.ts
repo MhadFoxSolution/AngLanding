78ecb543effaddea52aa5c619e61e7378105a7a0
AldoGzlVictoria <aldo.vic1408@hotmail.com>
2025-03-19 15:57:52 -0600

    Se protege las imagenes de descargas

--- Front-MHADFOX/src/pages/components/quienes-somos/quienes-somos.component.ts
+++ Front-MHADFOX/src/pages/components/quienes-somos/quienes-somos.component.ts
@@ -50,15 +50,15 @@ export class QuienesSomosComponent {
   carouselImages: string[] = [];
 
   cards = [
-    { image: 'images/abraham.jpeg', nombre: 'Jonathan Abraham ', descripcion: '[ESPECIALIDAD DE TI]' },
-    { image: 'images/aldo.jpg', nombre: 'Aldo Gonzalez', descripcion: '[ESPECIALIDAD DE TI]' },
-    { image: 'images/felipe.jpg', nombre: 'Felipe Segura', descripcion: '[ESPECIALIDAD DE TI]' },
+    { image: 'images/abraham.webp', nombre: 'Jonathan Abraham ', descripcion: '[ESPECIALIDAD DE TI]' },
+    { image: 'images/aldo.webp', nombre: 'Aldo Gonzalez', descripcion: '[ESPECIALIDAD DE TI]' },
+    { image: 'images/felipe.webp', nombre: 'Felipe Segura', descripcion: '[ESPECIALIDAD DE TI]' },
     { image: 'images/LogoMHADFOX.png', nombre: 'Mario Cervantes', descripcion: '[ESPECIALIDAD DE TI]' },
-    { image: 'images/alejandro.jpg', nombre: 'Alejandro Ramirez', descripcion: '[ESPECIALIDAD DE TI]' },
-    { image: 'images/hidai.jpg', nombre: 'Hidai Hernandez', descripcion: '[ESPECIALIDAD DE TI]' },
-    { image: 'images/david.jpg', nombre: 'David Galguera', descripcion: '[ESPECIALIDAD DE TI]' },
-    { image: 'images/oscar.jpg', nombre: 'Oscar Martinez', descripcion: '[ESPECIALIDAD DE TI]' },
-    { image: 'images/fernando.jpg', nombre: 'Fernando Medina', descripcion: '[ESPECIALIDAD DE TI]' }
+    { image: 'images/alejandro.webp', nombre: 'Alejandro Ramirez', descripcion: '[ESPECIALIDAD DE TI]' },
+    { image: 'images/hidai.webp', nombre: 'Hidai Hernandez', descripcion: '[ESPECIALIDAD DE TI]' },
+    { image: 'images/david.webp', nombre: 'David Galguera', descripcion: '[ESPECIALIDAD DE TI]' },
+    { image: 'images/oscar.webp', nombre: 'Oscar Martinez', descripcion: '[ESPECIALIDAD DE TI]' },
+    { image: 'images/fernando.webp', nombre: 'Fernando Medina', descripcion: '[ESPECIALIDAD DE TI]' }
   ];
 
   trackByFn(index: number, item: { image: string; nombre: string; descripcion: string }): any {
